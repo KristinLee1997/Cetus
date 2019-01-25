b9c213a68b86670d515e645a478c6010b4b93bc6
lihang <3215758767@qq.com>
2019-01-26 01:46:12 +0800

    init

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Cetus
 Delphinus-NetDisk(海豚网盘)存储服务
 
-1
-
 Delphinus-NetDisk(海豚网盘)项目:https://github.com/GoldArowana/Delphinus-NetDisk
+
---
+++ cetus-app/src/main/java/com/aries/cetus/app/Hello.java
@@ -0,0 +1,7 @@
+package com.aries.cetus.app;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
